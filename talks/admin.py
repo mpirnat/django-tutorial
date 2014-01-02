9016d1912def227397c136f070366d455209d123
Mike Pirnat <mpirnat@gmail.com>
2014-01-02 14:23:09 -0500

    Ex 10: Add relations to the Talks model

--- talks/admin.py
+++ talks/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from talks.models import Talk
+from talks.models import Category, AudienceSkillLevel
+from talks.models import Talk, TimeSlot, Location, TalkType
 
 
 admin.site.register(Talk)
+admin.site.register(TimeSlot)
+admin.site.register(Location)
+admin.site.register(TalkType)
+admin.site.register(Category)
+admin.site.register(AudienceSkillLevel)
--- talks/models.py
+++ talks/models.py
@@ -1,4 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class TimeSlot(models.Model):
+
+    slot = models.DateTimeField()
+
+    def __str__(self):
+        return self.slot.strftime("%A, %B %d, %Y @ %I:%M %p")
+
+    # Uncomment for Python 2
+    #def __unicode__(self):
+    #    return unicode(str(self))
+
+
+class Location(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    # Uncomment for Python 2
+    #def __unicode__(self):
+    #    return unicode(str(self))
+
+
+class TalkType(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    # Uncomment for Python 2
+    #def __unicode__(self):
+    #    return unicode(str(self))
+
+
+class AudienceSkillLevel(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    # Uncomment for Python 2
+    #def __unicode__(self):
+    #    return unicode(str(self))
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    # Uncomment for Python 2
+    #def __unicode__(self):
+    #    return unicode(str(self))
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
 
 class Talk(models.Model):
@@ -8,6 +72,13 @@ class Talk(models.Model):
     approved = models.NullBooleanField()
     recording_release = models.BooleanField(default=False)
 
+    talk_type = models.ForeignKey(TalkType)
+    category = models.ForeignKey(Category)
+    audience_skill_level = models.ForeignKey(AudienceSkillLevel)
+    location = models.ForeignKey(Location, blank=True, null=True)
+    time_slot = models.ForeignKey(TimeSlot, blank=True, null=True)
+    proposers = models.ManyToManyField(User)
+
     outline = models.TextField(blank=True)
     abstract = models.TextField(blank=True)
     notes = models.TextField(blank=True)
@@ -15,5 +86,6 @@ class Talk(models.Model):
     def __str__(self):
         return self.title
 
-    def __unicode__(self):
-        return unicode(str(self))
+    # Uncomment for Python 2
+    #def __unicode__(self):
+    #    return unicode(str(self))
