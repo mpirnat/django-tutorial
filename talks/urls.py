f22248326cb8868a53db47fe5b8e23819d952987
Mike Pirnat <mpirnat@gmail.com>
2014-01-02 15:48:24 -0500

    Ex 15: Using a generic UpdateView

--- talks/urls.py
+++ talks/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-from talks.views import TalkListView, TalkCreationView
+from talks.views import TalkListView, TalkCreationView, TalkUpdateView
 
 
 urlpatterns = patterns('talks.views',
     url(r'^$', TalkListView.as_view(), name='index'),
     url(r'^create/$', TalkCreationView.as_view(), name='create'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', TalkUpdateView.as_view(), name='update'),
 )
--- talks/views.py
+++ talks/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from talks.forms import TalkForm
 from talks.models import Talk
@@ -16,4 +16,20 @@ class TalkCreationView(CreateView):
 
     model = Talk
     form_class = TalkForm
-    success_url = '/accounts/profile/'
+
+    def get_success_url(self):
+        return resolve_url("accounts:profile")
+
+
+class TalkUpdateView(UpdateView):
+
+    form_class = TalkForm
+    queryset = Talk.objects.all()
+
+    def get_success_url(self):
+        return resolve_url("accounts:profile")
+
+    def get_extra_context_data(self, **kwargs):
+        contents = super(TalkUpdateView, self).get_extra_context_data(**kwargs)
+        context["editing"] = True
+        return context
